6c2f30cc3c62fb1942fa390fbd654b6a4a017608
Aitor de Santos <soyadsa06@gmail.com>
2026-02-05 10:13:49 +0100

    .

--- src/main/java/dev/adsa/controller/MainMenuController.java
+++ src/main/java/dev/adsa/controller/MainMenuController.java
@@ -9,19 +9,33 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.DialogPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 public class MainMenuController {
+
+    @FXML
+    private BorderPane menuBorderPane;
 
     @FXML
     private void showDialog() {
         try {
             FXMLLoader loader = new FXMLLoader(
-                getClass().getResource("/fxml/DialogUpdateView.fxml")
+                getClass().getResource("/view/DialogUpdateView.fxml")
             );
             DialogPane pane = loader.load();
+
             DialogUpdateController controller = loader.getController();
 
             Dialog<ButtonType> dialog = new Dialog<>();
+
             dialog.setDialogPane(pane);
+
+            // OWNER + MODALIDAD CORRECTA
+            Stage owner = (Stage) menuBorderPane.getScene().getWindow();
+            dialog.initOwner(owner);
+            dialog.initModality(Modality.APPLICATION_MODAL);
+
 
             controller.setData("Adsa");
 
