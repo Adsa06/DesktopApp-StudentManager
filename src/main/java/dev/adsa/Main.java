f7a70b69c3767195a08e56530c0422ec932d9b98
Aitor de Santos Amoros <soyadsa06@gmail.com>
2026-02-05 12:09:26 +0100

    .

--- src/main/java/dev/adsa/Main.java
+++ src/main/java/dev/adsa/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     }
     @Override
     public void start(Stage primaryStage) throws Exception {    
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/MainView.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/main_menu_view.fxml"));
         Parent root = loader.load();
 
         primaryStage.setTitle("Student Manager");
--- src/main/java/dev/adsa/controller/MainMenuController.java
+++ src/main/java/dev/adsa/controller/MainMenuController.java
@@ -9,7 +9,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.DialogPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 public class MainMenuController {
@@ -18,31 +20,24 @@ public class MainMenuController {
     private BorderPane menuBorderPane;
 
     @FXML
+    private AnchorPane overlay;
+
+    @FXML
+    public void initialize() {
+    }
+
+    @FXML
     private void showDialog() {
-        try {
-            FXMLLoader loader = new FXMLLoader(
-                getClass().getResource("/view/DialogUpdateView.fxml")
-            );
-            DialogPane pane = loader.load();
+        overlay.setVisible(true);
+        overlay.setDisable(false);
+        menuBorderPane.setDisable(true);
+    }
 
-            DialogUpdateController controller = loader.getController();
-
-            Dialog<ButtonType> dialog = new Dialog<>();
-
-            dialog.setDialogPane(pane);
-
-            // OWNER + MODALIDAD CORRECTA
-            Stage owner = (Stage) menuBorderPane.getScene().getWindow();
-            dialog.initOwner(owner);
-            dialog.initModality(Modality.APPLICATION_MODAL);
-
-
-            controller.setData("Adsa");
-
-            Optional<ButtonType> result = dialog.showAndWait();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    @FXML
+    private void closeDialog() {
+        overlay.setVisible(false);
+        menuBorderPane.setDisable(false);
+        overlay.setDisable(true);
     }
     
     @FXML
